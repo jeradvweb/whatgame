ea87031aebf1aa5e0def106451b6b7ff3aa7f43f
jeradvweb <31945581+jeradvweb@users.noreply.github.com>
2017-12-11 04:53:58 -0800

    v4
    Updated

---
+++ dist/switchgames.php
@@ -0,0 +1,45 @@
+<?php include 'partials/head.php'; ?>
+
+<div class="gamelistshelf">
+								<div class="gameshelf">
+									<a href="#"><div class="gamecontainer"></div></a>
+									<div class="gamecontainer"></div>
+									<div class="gamecontainer"></div>
+									<div class="gamecontainer"></div>
+									<div class="gamecontainer"></div>
+									<div class="gamecontainer"></div>
+									<div class="gamecontainer"></div>
+									<div class="gamecontainer"></div>
+									<div class="gamecontainer"></div>
+									<div class="gamecontainer"></div>
+									<div class="gamecontainer"></div>
+									<div class="gamecontainer"></div>
+									
+				
+								</div>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
--- dist/systems.php
+++ dist/systems.php
@@ -6,25 +6,25 @@
 		<div class="carouselmove">
 			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="false">
 				  <div class="carousel-inner" role="listbox">
-				    <div class="carousel-item active">
-				      <a href="#"><img class="d-block img-fluid consolesize" src="images/switch.png" alt="First slide"></a>
+				    <div class="carousel-item active consolesize">
+				      <a href="switchgames.php"><img class="d-block img-fluid consolesize" src="images/switch.png" alt="First slide"></a>
 				    </div>
 				    <div class="carousel-item consolesize">
-				      <a href="#"><img class="d-block img-fluid" src="images/ps4.png" alt="Second slide"></a>
+				      <a href="#"><img class="d-block img-fluid consolesize" src="images/ps4.png" alt="Second slide"></a>
 				    </div>
 				    <div class="carousel-item consolesize">
-				      <a href="#"><img class="d-block img-fluid" src="images/genesis.png" alt="Third slide"></a>
+				      <a href="#"><img class="d-block img-fluid consolesize" src="images/genesis.png" alt="Third slide"></a>
 				    </div>
 				  </div>
 
 
 
 
-				  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+				  <a class="carousel-control-prev carouselslidersl" href="#carouselExampleControls" role="button" data-slide="prev">
 				    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
 				    <span class="sr-only">Previous</span>
 				  </a>
-				  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+				  <a class="carousel-control-next carouselslidersr" href="#carouselExampleControls" role="button" data-slide="next">
 				    <span class="carousel-control-next-icon" aria-hidden="true"></span>
 				    <span class="sr-only">Next</span>
 				  </a>
@@ -33,13 +33,17 @@
 
 
 
-  			
+  		<div class="headerforpage">
+  			<img src="images/choose.png" alt="choose a system">
+  		</div>	
 
 
 
 
 
 </div>
+
+
 
 
 
